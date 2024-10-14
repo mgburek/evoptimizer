4acaa60557f71388e67212998c2af1a6c342b8ac
mgburek <mateusz.gburek2001@gmail.com>
2024-10-14 22:10:48 +0200

    Gene class refactoring

--- src/lib/population/gene.h
+++ src/lib/population/gene.h
@@ -1,9 +1,10 @@
-#pragma once
+#ifndef EVOPTIMIZER_POPULATION_GENE_H_
+#define EVOPTIMIZER_POPULATION_GENE_H_
 
 #include <limits>
 #include <bitset>
 
-template <typename Integral, typename Real, Real min, Real max>
+template <typename Integral, typename Real>
 class Gene
 {
     static_assert(std::is_integral_v<Integral> && std::is_unsigned_v<Integral>,
@@ -19,60 +20,52 @@ class Gene
     Integral _value;
 
 public:
+    inline static Real min = static_cast<Real>(-1.0);
+    inline static Real max = static_cast<Real>(1.0);
+
     using RealType = Real;
-    
-    Gene(Integral init_value = 0);
 
-    static Gene<Integral, Real, min, max> createRandom();
+    Gene(Integral init_value = 0) : _value{init_value}
+    {
+    }
 
-    Real toReal() const;
-    std::string toString() const;
-    std::string toBinaryString() const;
+    /**
+     * Creates a random gene
+     */
+    static Gene<Integral, Real> createRandom()
+    {
+        return Gene<Integral, Real>(gen());
+    }
+
+    /**
+     * Converts gene's encoded value to real (floating point type) number
+     */
+    Real toReal() const
+    {
+        return min + (static_cast<Real>(_value) * UINT_SCALER) * (max - min);
+    }
+
+    /**
+     * Converts gene to std::string representing it's real value
+     */
+    std::string toString() const
+    {
+        return std::to_string(toReal());
+    }
+
+    /**
+     * Converts gene to std::string representing it's binary value
+     */
+    std::string toBinaryString() const
+    {
+        std::bitset<sizeof(Integral) * 8> binary(_value);
+        return binary.to_string();
+    }
 };
 
 // ============================================================
 
-template <typename Integral, typename Real, Real min, Real max>
-IntRandomGenerator<Integral> Gene<Integral, Real, min, max>::gen(0, std::numeric_limits<Integral>::max());
+template <typename Integral, typename Real>
+IntRandomGenerator<Integral> Gene<Integral, Real>::gen(0, std::numeric_limits<Integral>::max());
 
-template <typename Integral, typename Real, Real min, Real max>
-Gene<Integral, Real, min, max>::Gene(Integral init_value) : _value{init_value}
-{
-}
-
-/**
- * Creates a random gene
- */
-template <typename Integral, typename Real, Real min, Real max>
-Gene<Integral, Real, min, max> Gene<Integral, Real, min, max>::createRandom()
-{
-    return Gene<Integral, Real, min, max>(gen());
-}
-
-/**
- * Converts gene's encoded value to real (floating point type) number
- */
-template <typename Integral, typename Real, Real min, Real max>
-Real Gene<Integral, Real, min, max>::toReal() const
-{
-    return min + (static_cast<Real>(_value) * UINT_SCALER) * (max - min);
-}
-
-/**
- * Converts gene to std::string representing it's real value
- */
-template <typename Integral, typename Real, Real min, Real max>
-std::string Gene<Integral, Real, min, max>::toString() const
-{
-    return std::to_string(toReal());
-}
-
-/**
- * Converts gene to std::string representing it's binary value
- */
-template <typename Integral, typename Real, Real min, Real max>
-std::string Gene<Integral, Real, min, max>::toBinaryString() const
-{
-    std::bitset<sizeof(Integral) * 8> binary(_value);
-    return binary.to_string();
-}
+#endif
