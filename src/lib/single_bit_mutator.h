a378614076af0cc530d1c8f1e6c45dc884ba333f
mgburek <mateusz.gburek2001@gmail.com>
2024-10-24 15:54:43 +0200

    mutator simplicifcation

--- src/lib/single_bit_mutator.h
+++ src/lib/single_bit_mutator.h
@@ -5,54 +5,50 @@
 
 #include <generators.h>
 
-/**
- * Mutates at most one bit of each individual according to mutation chance
- */
-template <typename I, typename R, size_t genes_num, size_t population_size>
-class SingleBitMutator
+namespace Evoptimizer
 {
-    using GenerationType = Generation<I, R, genes_num, population_size>;
-    using IndividualType = Individual<I, R, genes_num>;
-    using GeneType = Gene<I, R>;
-
-    static Random::IntGenerator<size_t> mutation_point_gen;
-
-    const double _mutation_chance;
-
-public:
-    SingleBitMutator(double mutation_chance)
-        : _mutation_chance(mutation_chance) {}
-
-    GenerationType operator()(const GenerationType &generation) const
+    /**
+     * Mutates at most one bit of each individual according to mutation chance
+     */
+    template <typename I, typename R, size_t genes_num, size_t population_size>
+    class SingleBitMutator
     {
-        GenerationType new_generation(generation);
-        auto &new_population = new_generation.mutablePopulation();
+        static Random::IntGenerator<size_t> mutation_point_gen;
 
-        auto mutate = [this](IndividualType &individual)
+        const double _mutation_chance;
+
+    public:
+        SingleBitMutator(double mutation_chance)
+            : _mutation_chance(mutation_chance) {}
+
+        Generation<I, R, genes_num, population_size> operator()(const Generation<I, R, genes_num, population_size> &generation) const
         {
-            if (Random::zero_one() < _mutation_chance)
+            Generation<I, R, genes_num, population_size> mutated(generation);
+
+            auto mutate = [this](Individual<I, R, genes_num> &individual)
             {
-                auto &genes = individual.mutableGenes();
-                size_t mutation_point = mutation_point_gen();
-                auto mutated_gene_num = mutation_point / GeneType::bitsNum();
+                if (Random::zero_one() < _mutation_chance)
+                {
+                    size_t mutation_point = mutation_point_gen();
+                    auto mutated_gene_num = mutation_point / lengthInBits<I, 1>();
 
-                auto mask = 0x1 << (mutation_point % GeneType::bitsNum());
-                auto gene_v = genes[mutated_gene_num].get();
-                genes[mutated_gene_num].set(gene_v ^ mask);
-            }
-        };
+                    auto mask = 0x1 << (mutation_point % lengthInBits<I, 1>());
+                    auto gene = individual[mutated_gene_num].get();
+                    individual[mutated_gene_num].set(gene ^ mask);
+                }
+            };
 
-        std::for_each(new_population.begin(), new_population.end(), mutate);
+            std::for_each(mutated.begin(), mutated.end(), mutate);
+            return mutated;
+        }
+    };
 
-        return new_generation;
-    }
-};
+    // ============================================================
 
-// ============================================================
+    template <typename I, typename R, size_t genes_num, size_t population_size>
+    Random::IntGenerator<size_t>
+        SingleBitMutator<I, R, genes_num, population_size>::mutation_point_gen(1, lengthInBits<I, genes_num>() - 1);
 
-template <typename I, typename R, size_t genes_num, size_t population_size>
-Random::IntGenerator<size_t>
-    SingleBitMutator<I, R, genes_num, population_size>::
-        mutation_point_gen(1, Individual<I, R, genes_num>::bitsNum() - 1);
+}
 
 #endif
