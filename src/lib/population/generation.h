3d2c6a23447a2fe1b0b861e1ea00d2e35cf9a389
mgburek <mateusz.gburek2001@gmail.com>
2024-10-24 13:07:26 +0200

    Generation simplification

--- src/lib/population/generation.h
+++ src/lib/population/generation.h
@@ -1,65 +1,37 @@
 #ifndef EVOPTIMIZER_POPULATION_GENERATION_H_
 #define EVOPTIMIZER_POPULATION_GENERATION_H_
 
-#include <cstdlib>
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <sstream>
 #include <string>
 
-template <typename I, typename R, size_t genes_num, size_t population_size>
-class Generation
+#include "individual.h"
+
+namespace Evoptimzer
 {
-    using IndividualType = Individual<I, R, genes_num>;
-    std::array<IndividualType, population_size> _population;
+    template <typename I, typename R, size_t genes_num, size_t population_size>
+    using Generation = std::array<Individual<I, R, genes_num>, population_size>;
 
-public:
-    Generation(const std::array<IndividualType, population_size> &population)
-        : _population(population) {}
-
-    Generation()
-        : _population() {}
-
-    Generation(const Generation<I, R, genes_num, population_size> &other)
-        : _population(other._population) {}
-
-    static Generation<I, R, genes_num, population_size> createRandom();
-
-    std::string toString() const;
-
-    std::array<IndividualType, population_size> &mutablePopulation()
+    template <typename I, typename R, size_t genes_num, size_t population_size>
+    Generation<I, R, genes_num, population_size> createRandom()
     {
-        return _population;
+        Generation<I, R, genes_num, population_size> generation;
+        std::generate(generation.begin(), generation.end(), []()
+                      { return createRandom<I, R, genes_num>(); });
+        return generation;
     }
 
-    const std::array<IndividualType, population_size> &population() const
+    template <typename I, typename R, size_t genes_num, size_t population_size>
+    std::string toString(const Generation<I, R, genes_num, population_size> &generation)
     {
-        return _population;
+        std::ostringstream str_stream;
+        for (const Individual<I, R, genes_num> &ind : generation)
+            str_stream << Evoptimzer::toString(ind) << std::endl;
+
+        return str_stream.str();
     }
-};
-
-// ============================================================
-
-/**
- * Creates a random population
- */
-template <typename I, typename R, size_t genes_num, size_t population_size>
-Generation<I, R, genes_num, population_size> Generation<I, R, genes_num, population_size>::createRandom()
-{
-    Generation<I, R, genes_num, population_size> generation;
-    std::generate(generation.mutablePopulation().begin(), generation.mutablePopulation().end(), []()
-                  { return IndividualType::createRandom(); });
-    return std::move(generation);
-}
-
-/**
- * Converts an indivdual to space separated std::string
- */
-template <typename I, typename R, size_t genes_num, size_t population_size>
-std::string Generation<I, R, genes_num, population_size>::toString() const
-{
-    std::ostringstream str_stream;
-    for (const IndividualType &individual : _population)
-        str_stream << individual.toString() << std::endl;
-
-    return str_stream.str();
 }
 
 #endif
